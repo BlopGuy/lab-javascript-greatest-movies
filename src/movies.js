8ad8e5a0c0a60aa7bc552388f2633485d6ec7b18
BlopGuy <gvieira.assuntos@outlook.pt>
2021-01-14 16:28:58 +0000

    iterations 3 and 4 complete

--- src/movies.js
+++ src/movies.js
@@ -24,8 +24,42 @@ let howManyMovies = ((movieArray) => {
 });
 
 // Iteration 3: All rates average - Get the average of all rates with 2 decimals
+let ratesAverage = ((movieArray) => {
+    if(movieArray.length === 0) {
+        return 0;
+    }
+    let rateAvg = parseFloat((movieArray.reduce((accumulator, currentValue) => {
+        if(currentValue.rate !== undefined) {
+            return accumulator + currentValue.rate;
+        } else {
+            return accumulator;
+        }
+    }, 0)/movieArray.length).toFixed(2));
+
+    return rateAvg;
+
+});
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
+
+let dramaMoviesRate = ((movieArray) => {
+    let dramaMovieNumber = 0;
+
+    let rateDramaAvg = parseFloat((movieArray.reduce((accumulator, currentValue) => {
+        if(currentValue.genre.includes("Drama")) {
+            dramaMovieNumber++;
+            return accumulator + currentValue.rate;
+        } else {
+            return accumulator;
+        }
+    }, 0)/dramaMovieNumber).toFixed(2));
+
+    if(dramaMovieNumber === 0) {
+        return dramaMovieNumber;
+    } else {
+        return rateDramaAvg;
+    }
+});
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
 
